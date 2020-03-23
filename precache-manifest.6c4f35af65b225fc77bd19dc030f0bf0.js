7ce03ceddce93475ccaf7979831f900d29061b71
Kristian Lentino <k.lentino@libero.it>
2020-03-23 22:02:40 +0100

    Updates

---
+++ precache-manifest.6c4f35af65b225fc77bd19dc030f0bf0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8556665348a3bebc4253edb1ec1f2e88",
+    "url": "/covid-stats/index.html"
+  },
+  {
+    "revision": "b45dff81878416732765",
+    "url": "/covid-stats/static/css/main.d3cdd6b3.chunk.css"
+  },
+  {
+    "revision": "e1afe2040b9d51bf3f20",
+    "url": "/covid-stats/static/js/2.815434ee.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/covid-stats/static/js/2.815434ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b45dff81878416732765",
+    "url": "/covid-stats/static/js/main.07660052.chunk.js"
+  },
+  {
+    "revision": "7b4131d014be774bea13",
+    "url": "/covid-stats/static/js/runtime-main.0c1266b4.js"
+  }
+]);
